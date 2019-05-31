ea34dcdfaafb575f380ae5e6c6e415082ed374b3
8whidayat <51222381+8whidayat@users.noreply.github.com>
2019-05-31 11:08:54 +0700

    Add files via upload

---
+++ Windows Simulator/Windows Simulator/App/Firefox.vb
@@ -0,0 +1,7 @@
+﻿Public Class Firefox
+
+    Private Sub Firefox_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Me.MdiParent = Form1
+
+    End Sub
+End Class
